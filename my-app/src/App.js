c891bec2092c48b293116262f9fa145c9c90207c
jcharp <61035559+jcharp@users.noreply.github.com>
2020-09-16 21:36:56 -0400

    basic login form. no functionality yet

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Login from "./Components/Login";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+
         <h1>ANYWHERE FITNESS</h1>
+        <Login />
+
       </header>
     </div>
   );
--- my-app/src/Components/Login.js
+++ my-app/src/Components/Login.js
@@ -1,8 +1,46 @@
+import React, { useState } from "react";
 
 
 function Login(){
+
+    const [user, setUser]= useState({
+        username: "",
+        password: "",
+
+    })
+
+
+
+
     return(
-        console.log("from Login")
+        <div className="App">
+
+            <h3>Login</h3>
+            <form>
+                <label htmlFor="username">User Name: </label>
+                <input
+                    type="text"
+                    placeholder="Enter User Name"
+                    name="username"
+                    value={user.username}
+                    />
+                <br/>
+                <label htmlFor="password">Password: </label>
+                <input
+                    type="password"
+                    placeholder="Enter Password"
+                    name="password"
+                    value={user.password}
+                    />
+
+                <br/>    
+
+                 <button type="submit">Submit</button>   
+
+            </form>
+
+        </div>
+        
 
     )
 }
