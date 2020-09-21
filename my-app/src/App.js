3418969113938ef0d0c601dcc52dca770d945f2f
Claeb1mb <52614744+Claeb1mb@users.noreply.github.com>
2020-09-21 12:44:32 -0400

    home page update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,19 +2,27 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// Material ui imports
+import { ThemeProvider } from "@material-ui/core";
+
+// Component Imports
 import HomePage from "./Components /Home_Components/HomePage";
+import Theme from "./Components /Home_Components/Theme";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route exact="/" component={HomePage} />
-          <Route exact="/users" />
-          <Route exact="/register" />
-          <Route exact="/Login" />
-        </Switch>
-      </Router>
+      <ThemeProvider theme={Theme}>
+        <Router>
+          <Switch>
+            <Route exact="/" component={HomePage} />
+            <Route exact="/users" />
+            <Route exact="/register" />
+            <Route exact="/Login" />
+          </Switch>
+        </Router>
+      </ThemeProvider>
     </div>
   );
 }
