d5a60fe9fbede83cdfac58db47dfe14cd6f371ac
jcharp <61035559+jcharp@users.noreply.github.com>
2020-09-23 21:01:03 -0400

    committing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ClientRegistration from "./Components/Client_Registration";
+import ClientLogin from './Components/Client_Login';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h1>ANYWHERE FITNESS</h1>
+       
+
+        <ClientRegistration/>
+
+        <ClientLogin/>
+      
+
       </header>
     </div>
   );
