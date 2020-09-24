52ec13922bdfec77fc7ecee7ffe112e540343e8a
Claeb1mb <52614744+Claeb1mb@users.noreply.github.com>
2020-09-24 08:42:28 -0400

    initial push

--- my-app/src/Components/Login_Components/Login.js
+++ my-app/src/Components/Login_Components/Login.js
@@ -12,19 +12,8 @@ import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
+import { axiosWithAuth } from "../../Utils/axiosWithAuth";
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
 const useStyles = makeStyles((theme) => ({
   root: {
     height: "100vh",
@@ -59,8 +48,35 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+// helper functions
+
 const LoginForm = () => {
   const classes = useStyles();
+
+  const [login, setLogin] = useState({
+    username: "client3",
+    password: "password3",
+  });
+
+  // helper functions
+  const loggingIn = (e) => {
+    axiosWithAuth()
+      .get("http://localhost5000/api/client/login")
+      .then((res) => {
+        console.log("res", res);
+        localStorage.setItem("token", res.data.payload);
+        // Set up a route to push to once the client logs in successfully
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const changeHandler = (e) => {
+    setLogin({
+      ...login,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <div>
       <Grid container component="main" className={classes.root}>
@@ -69,19 +85,26 @@ const LoginForm = () => {
         <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
           <div className={classes.paper}>
             <Avatar className={classes.avatar} />
-            <form className={classes.form} noValidate>
+            <form
+              onClick={() => loggingIn()}
+              className={classes.form}
+              noValidate
+            >
               <TextField
+                onChange={changeHandler}
                 variant="outlined"
                 margin="normal"
                 required
                 fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
+                id="username"
+                label="Username"
+                name="username"
+                autoComplete="username"
+                value={login.username}
                 autoFocus
               />
               <TextField
+                onChange={changeHandler}
                 variant="outlined"
                 margin="normal"
                 required
@@ -90,6 +113,7 @@ const LoginForm = () => {
                 label="Password"
                 type="password"
                 id="password"
+                value={login.password}
                 autoComplete="current-password"
               />
 
@@ -102,7 +126,6 @@ const LoginForm = () => {
                 fullWidth
                 variant="contained"
                 color="primary"
-                className={classes.submit}
               >
                 Sign Up
               </Button>
